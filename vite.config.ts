603ed55783ca2b208670b2082cca21ed8a33eca3
Pranay Reddy <pranay.reddy@mtxb2b.com>
2025-04-20 01:27:02 +0530

    k

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
     outDir: "dist",
     assetsDir: "assets",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
   },
   optimizeDeps: {
     exclude: ["lucide-react"],
